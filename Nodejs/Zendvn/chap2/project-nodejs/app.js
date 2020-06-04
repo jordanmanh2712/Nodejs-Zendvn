cacb0bca05b6b5ee099e2a0fa859069469d9c68b
kieumanh <jordanmanh2712@gmail.com>
2020-06-04 13:08:08 +0700

    Update Nodejs Level 1 - chap 2 - Tash: prefixAdmin

---
+++ Nodejs/Zendvn/chap2/project-nodejs/app.js
@@ -0,0 +1,51 @@
+//File system
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+//File 3rd services
+var expressLayouts = require('express-ejs-layouts');
+
+// const systemConfig = require('./configs/system');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', 'layout');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Prefix admin local variable
+app.locals.prefixAdmin = 'admin123';
+
+//Setup router front end va back end
+app.use('/', require('./routes/frontend/index'));
+app.use('/admin123', require('./routes/backend/index'));
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
---
+++ Nodejs/Zendvn/chap2/project-nodejs/routes/backend/items.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('pages/items/list', { pageTitle: 'Item Page' });
+});
+
+router.get('/list', function(req, res, next) {
+  res.render('pages/items/list', { pageTitle: 'Item List Page' });
+});
+
+router.get('/add', function(req, res, next) {
+  res.render('pages/items/add', { pageTitle: 'Item Add Page' });
+});
+
+module.exports = router;
